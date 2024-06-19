04d7ca4d3a4bf2e729c7b0d6a97d6478085e6a7f
Leonardo Gazola Medeiros <leonardo.gazolamedeiros@gmail.com>
2024-06-19 00:56:12 -0300

    senha pra aws

--- backend/database.js
+++ backend/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'password',
+    user: 'leo',
+    password: 'senha',
     database: 'sabha'
 });
 
